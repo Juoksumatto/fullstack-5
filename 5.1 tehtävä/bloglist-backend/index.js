5ccf295fc5ca5eb7b7d39d9dc627fb8bf4ed866d
onni <holttaonni@gmail.com>
2026-04-07 09:29:40 +0300

    Update index.js

--- 5.1 tehtävä/bloglist-backend/index.js
+++ 5.1 tehtävä/bloglist-backend/index.js
@@ -11,20 +11,25 @@ const users = [
 ]
 
 const blogs = [
-  { id: 1, title: 'likes', author: 'Onni Hölttä', url: 'https://github.com/Juoksumatto/Fullstack-5', likes: 72376 },
+  { id: 1, title: 'this is a blog post yup defineadly true dont doubt it its true', author: 'Onni Hölttä', url: 'https://github.com/Juoksumatto/Fullstack-5', likes: 72376 },
 ]
 
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body
 
-  const user = users.find(u => u.username === username && u.password === password)
-  
+  let user = users.find(u => u.username === username)
+
   if (!user) {
-    return res.status(401).json({ error: 'invalid username or password' })
+    user = { username, name: username, password }
+    users.push(user)
+  } else {
+    if (user.password !== password) {
+      return res.status(401).json({ error: 'invalid username or password' })
+    }
   }
 
   const token = `token_${username}_${Date.now()}`
-  
+
   res.json({
     token,
     username: user.username,
@@ -33,12 +38,6 @@ app.post('/api/login', (req, res) => {
 })
 
 app.get('/api/blogs', (req, res) => {
-  const token = req.get('authorization')
-  
-  if (!token) {
-    return res.status(401).json({ error: 'token missing' })
-  }
-
   res.json(blogs)
 })
 
